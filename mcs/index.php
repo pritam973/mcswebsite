b30841eb33fdeeb79ffbc633188cd4508a9a835d
pritam973 <sahamamoni973@gmail.com>
2026-04-03 13:54:18 +0530

    Update enrollment

--- mcs/index.php
+++ mcs/index.php
@@ -22,7 +22,7 @@ mysqli_close($conn);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Marine Command Squad - Portal</title>
-    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="index.css">
     <style>
         * {
             margin: 0;
